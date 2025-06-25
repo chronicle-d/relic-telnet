03d2858fb8588f1bc6eda2108dc8b02fee474aba
Noam Alum <noam.alum@gmail.com>
2025-06-25 17:16:22 +0300

    Removed unnecessary try catch in tester

--- src/rtelnet.cpp
+++ src/rtelnet.cpp
@@ -4,10 +4,7 @@
  */
 
 #include "rtelnet.hpp"
-
-#include <exception>
 #include <iostream>
-#include <vector>
 
 int main(int argc, char *argv[]) {
   if (argc < 2) {
@@ -19,28 +16,23 @@ int main(int argc, char *argv[]) {
 
   Session._address = argv[1];
   Session._port = std::atoi(argv[2]);
-  Session._username = "toker";
+  Session._username = "example";
   Session._password = "[nM3r2>W31S_";
 
-  try {
-    unsigned int connectSuccess = Session.Connect();
-    if (connectSuccess != RTELNET_SUCCESS) { std::cerr << "[CONNECTION_ERROR]: " << rtnt::readError(connectSuccess) << "\n"; return 1; }
+  unsigned int connectSuccess = Session.Connect();
+  if (connectSuccess != RTELNET_SUCCESS) { std::cerr << "[CONNECTION_ERROR]: " << rtnt::readError(connectSuccess) << "\n"; return 1; }
 
-    unsigned int loginSuccess = Session.Login();
-    if (loginSuccess != RTELNET_SUCCESS) { std::cerr << "[LOGGIN_ERROR]: " << rtnt::readError(loginSuccess) << "\n"; return 1; }
+  unsigned int loginSuccess = Session.Login();
+  if (loginSuccess != RTELNET_SUCCESS) { std::cerr << "[LOGGIN_ERROR]: " << rtnt::readError(loginSuccess) << "\n"; return 1; }
 
-    unsigned int flushSuccess = Session.FlushBanner();
-    if (flushSuccess != RTELNET_SUCCESS) { std::cerr << "[FLUSH_BANNER_ERROR]: " << rtnt::readError(loginSuccess) << "\n"; return 1; }
+  unsigned int flushSuccess = Session.FlushBanner();
+  if (flushSuccess != RTELNET_SUCCESS) { std::cerr << "[FLUSH_BANNER_ERROR]: " << rtnt::readError(loginSuccess) << "\n"; return 1; }
 
-    std::string buffer;
-    unsigned int execSuccess = Session.Execute("ls -al", buffer);
-    if (execSuccess != RTELNET_SUCCESS) { std::cerr << "[EXEC_ERROR]: " << rtnt::readError(loginSuccess) << "\n"; return 1; }
+  std::string buffer;
+  unsigned int execSuccess = Session.Execute("ls -al", buffer);
+  if (execSuccess != RTELNET_SUCCESS) { std::cerr << "[EXEC_ERROR]: " << rtnt::readError(loginSuccess) << "\n"; return 1; }
 
-    std::cout << buffer << "\n";
-
-  } catch (std::exception &e) {
-    std::cerr << "[ERROR]: " << e.what() << "\n";
-  }
+  std::cout << buffer << "\n";
 
   Session._tcp.Close();
   return 0;
